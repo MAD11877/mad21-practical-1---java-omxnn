bf55a162102ba615d03653465feb3a9e2e9c33b8
Minn Ong <80464096+omxnn@users.noreply.github.com>
2021-04-21 23:29:04 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -26,7 +26,7 @@ public class Question4
     {
       for (int j = 1; j <= i; ++j) 
       {
-        System.out.print("* ");
+        System.out.print("*");
       }
       System.out.println();
     }
